f1bb54c76b544cd661b55a36c274d02dbaa0ba76
DevDevil <captainaydin11@gmail.com>
2024-02-27 22:49:42 -0500

    Delete config.js file

--- config.js
+++
@@ -1,7 +0,0 @@
-const config = {
-  challenge: true, // Turn this to true if you want passwords on.
-  users: {
-    devdevil: '123', // Username is 'devdevil'. Password is 'password'.
-  }
-}
-export default config
