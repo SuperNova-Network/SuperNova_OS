9d9fe23b80412072c9bd168b9727b0c5a4dd0ed9
DevDevil <captainaydin11@gmail.com>
2025-04-24 20:48:41 -0400

    Refactor keyboard shortcut handling for improved readability and added canvas quick login shortcut

--- public/resources/scripts/shortcuts.js
+++ public/resources/scripts/shortcuts.js
@@ -1,5 +1,18 @@
-document.addEventListener('keydown', function(event) {
-    if (event.altKey && event.key === 'g') {
-        window.location.href = "https://classroom.google.com";
+document.addEventListener('keydown', handleShortcut);
+
+function handleShortcut(event) {
+    if (event.altKey) {
+        switch (event.key) {
+            case 'g':
+                navigateTo("https://classroom.google.com");
+                break;
+            case 'c':
+                navigateTo("https://samlidp.clever.com/saml-canvas/signon");
+                break;
+        }
     }
-});
+}
+
+function navigateTo(url) {
+    window.location.href = url;
+}
